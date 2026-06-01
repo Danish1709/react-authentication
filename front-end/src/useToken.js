bc0ae7bc3781a5c132188b61e7dcea4342c7c890
Danish Khan <khandanishkhan1709@gmail.com>
2026-06-01 21:14:19 +0530

    adding usetoken and useuser custom hook

---
+++ front-end/src/useToken.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+export const useToken=()=>{
+  const [token,setTokenInternal]=useState(()=>{
+    return localStorage.getItem('token');
+  });
+   const setToken= newtoken=>{
+    localStorage.setItem('token',newtoken);
+    setTokenInternal(newtoken);
+   }
+   return [token,setToken];
+}
---
+++ front-end/src/useUser.js
@@ -0,0 +1,23 @@
+import { useState,useEffect } from "react";
+import { useToken } from "./UseToken";
+export const useUser=()=>{
+  const [token]=useToken();
+  const getPayloadFromToken=token=>{
+    const encodedPayload= token.split('.')[1];
+    return JSON.parse(atob(encodedPayload));
+  }
+  const[user,setUser]=useState(()=>{
+    if(!token) return null;
+    return getPayloadFromToken(token);
+
+  });
+  useEffect(()=>{
+if (!token){
+  return setUser(null);
+}
+else{
+  setUser(getPayloadFromToken(token));
+}
+  },[token]);
+return user;
+}
